0693015f50d5bfffb15a22c2a671dca7775f482a
mario130 <mario.yonan.work@gmail.com>
2020-08-06 20:20:04 +0200

    deploy

---
+++ precache-manifest.784f706e27f8026981499bf5a3c2a4f7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "278d13cb12fb059398c8",
+    "url": "/todo-app-deploy/css/about.8b5fefc0.css"
+  },
+  {
+    "revision": "1ceb139674024970fc1d",
+    "url": "/todo-app-deploy/css/app.c14b6de2.css"
+  },
+  {
+    "revision": "9435881f32556a0d653c",
+    "url": "/todo-app-deploy/css/chunk-vendors.63908016.css"
+  },
+  {
+    "revision": "3a593335409365634cb2d25ad9bf0226",
+    "url": "/todo-app-deploy/img/av-1.3a593335.png"
+  },
+  {
+    "revision": "7d8e4b2065762975a754e6499e6ce601",
+    "url": "/todo-app-deploy/index.html"
+  },
+  {
+    "revision": "278d13cb12fb059398c8",
+    "url": "/todo-app-deploy/js/about.5ee1e0c5.js"
+  },
+  {
+    "revision": "1ceb139674024970fc1d",
+    "url": "/todo-app-deploy/js/app.61147e09.js"
+  },
+  {
+    "revision": "9435881f32556a0d653c",
+    "url": "/todo-app-deploy/js/chunk-vendors.17ff52e4.js"
+  },
+  {
+    "revision": "9c7aacd27376619d6896d4327e20c894",
+    "url": "/todo-app-deploy/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/todo-app-deploy/robots.txt"
+  }
+]);
